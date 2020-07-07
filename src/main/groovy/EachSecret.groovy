e72b4e2105fb704d1cdfafd6054443acf40ea136
Pierre Ernst <pierre.ernst@elastic.co>
2020-07-07 12:02:16 -0400

    Add sample files

---
+++ src/main/groovy/EachSecret.groovy
@@ -0,0 +1,17 @@
+class EachSecret {
+    String red_herring = "DEADBEEF"
+
+    String base64_secret = "c2VjcmV0IG1lc3NhZ2Ugc28geW91J2xsIG5ldmVyIGd1ZXNzIG15IHBhc3N3b3Jk"
+    String hex_secret = "8b1118b376c313ed420e5133ba91307817ed52c2"
+    String basic_auth = "http://username:whywouldyouusehttpforpasswords@example.com"
+
+    String aws_access_key = "AKIAIOSFODNN7EXAMPLE"
+    String aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+    String base64_secret2 = "c2VjcmV0IG1lc3NhZ2Ugc28geW91J2xFFFTTGd1ZXNzIG15IHBhc3N3b3Jk"
+
+    String secret = "notHighEnoughEntropy"
+
+    String SUPER_SEECRET_VALUE = "c3VwZXIgbG9uZyBzdHJpbmcgc2hvdWxkIGNhdXNlIGVub3VnaCBlbnRyb3B5"
+    String VERY_SECRET_TOO = "f6CGV4aMM9zedoh3OUNbSakBymo7yplB"  // pragma: allowlist secret
+}
